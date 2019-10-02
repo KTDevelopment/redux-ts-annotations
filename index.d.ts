92c39a6d3caaecb9775551da1d62e929f90ceb0a
kevincodecookies <kevin.thuermann@codecookies.com>
2019-10-03 00:40:32 +0200

    add index.d.ts release v1.0.3

---
+++ index.d.ts
@@ -0,0 +1,15 @@
+// Type definitions for redux-ts-annotations
+// Project: redux-ts-annotations
+// Definitions by: Kevin Thürmann <kevin.thuermann@web.de>
+
+/*~ If this module has methods, declare them as functions like so.
+ */
+
+import {ActionCreatorDefinition} from "redux-ts";
+
+export function ConnectProp(propKeyOrModifier: string | StateModifier)
+export function ConnectToStore(propType: { new(): any })
+export function connectToStore(propType: { new(): any }, compType: any)
+export function ConnectAction(actionCreator: ActionCreatorDefinition<any, any>)
+
+type StateModifier = (state: any) => any
